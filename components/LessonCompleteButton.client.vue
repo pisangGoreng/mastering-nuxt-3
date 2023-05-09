12a8c09d7421214ce3be20a65cf1621f86b297a0
endy <7ofpentacles@gmail.com>
2023-05-09 12:54:44 +0700

    test

---
+++ components/LessonCompleteButton.client.vue
@@ -0,0 +1,40 @@
+<template>
+  <label
+    class="rounded text-white font-bold py-2 px-4 cursor-pointer"
+    :class="{
+      'bg-green-500': modelValue,
+      'bg-gray-500': !modelValue,
+    }"
+  >
+    <!-- 
+      emit -> untuk execute function dari parent nya
+
+      di parent -> @update:model-value="toggleComplete" 
+      di children perlu di define dlu -> defineEmits(["update:modelValue"]);
+    
+     -->
+    <input
+      type="checkbox"
+      :value="modelValue"
+      @input="() => $emit('update:modelValue', !modelValue)"
+      class="hidden"
+    />
+    {{ modelValue ? "Completed!" : "Mark as complete" }}
+  </label>
+</template>
+
+<script setup>
+defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false,
+  },
+});
+defineEmits(["update:modelValue"]);
+</script>
+
+<style scoped>
+::selection {
+  display: none;
+}
+</style>
